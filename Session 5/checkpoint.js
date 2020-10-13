31922fd5d4f26485a67826b38d62fea0945cb818
dangminhduc <dangminhduca3@gmail.com>
2020-10-13 20:56:57 +0700

    add checkpoint

---
+++ Session 5/checkpoint.js
@@ -0,0 +1,11 @@
+
+// Bai 1
+let a = Math.floor((Math.random() * 2)); // Math.floor de lam tron so; Math random tao 1 day random
+console.log(a);
+
+
+//Bai 2
+
+let b = [2, 5, 6, 9, 10];
+let random = b[Math.floor(Math.random() * b.length)];   // lay 1 phan tu ran dom trong mang
+console.log(random);
