ae4a5f03bb9f3c2a7b501630f5f325cfc2a7f1cf
dangminhduc <dangminhduca3@gmail.com>
2020-10-14 19:31:23 +0700

    update session 5

--- Session 5/checkpoint.js
+++ Session 5/checkpoint.js
@@ -1,11 +1,11 @@
 
 // Bai 1
-let a = Math.floor((Math.random() * 2)); // Math.floor de lam tron so; Math random tao 1 day random
-console.log(a);
+// let a = Math.floor((Math.random() * 2)); // Math.floor de lam tron so; Math random tao 1 day random ; Mathrandom() * 2 la de tao 1 day so ngau nhien co 2 so;
+// console.log(a);
 
 
 //Bai 2
 
-let b = [2, 5, 6, 9, 10];
-let random = b[Math.floor(Math.random() * b.length)];   // lay 1 phan tu ran dom trong mang
-console.log(random);
+// let b = [2, 5, 6, 9, 10];
+// let random = b[Math.floor(Math.random() * b.length)];   // lay 1 phan tu ran dom trong mang
+// console.log(random);
