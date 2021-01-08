9dfc5e183624139dbe7858d4a657c79ac1f31143
dangminhduc <dangminhduca3@gmail.com>
2021-01-08 19:51:30 +0700

    update firebase method

--- CI/Session_4_Firebase/index.js
+++ CI/Session_4_Firebase/index.js
@@ -5,28 +5,29 @@ getManyDocuments();
 
 
 async function getOneDocument() {
-  const docId = 'xA8Z8EaUx5oWDhNTs9to';
-  //doc chỉ tìm được ID
-  let res = await db.collection('users').doc(docId).get();
-  let user = getDataFromDoc(res);
-  console.log(user);
+    const docId = 'xA8Z8EaUx5oWDhNTs9to';
+    //doc chỉ tìm được ID
+    let res = await db.collection('users').doc(docId).get();
+    let user = getDataFromDoc(res);
+    console.log(user);
 }
 
 //Read Multi
 async function getManyDocuments() {
-  const res = await db.collection('users').get();
-  console.log(res.docs);
-  const users = getDataFromDocs(res.docs);
-  console.log(users)
+    const res = await db.collection('users').where('email', 'array-contains', 'email1').get();
+    //Field là array dùng array-contains trong where
+    
+    const users = getDataFromDocs(res.docs);
+    console.log(users)
   //Bắt buộc phải .docs
- 
-  
+
+
 }
 
 function getDataFromDoc(res) {
-  const user = res.data();
-  user.id = res.id;
-  return user;
+    const user = res.data();
+    user.id = res.id;
+    return user;
 }
 
 
@@ -37,7 +38,7 @@ function getDataFromDocs(docs) {
   // }
   // return arr
 
-  return docs.map(getDataFromDoc)
+    return docs.map(getDataFromDoc)
 
 }
 
@@ -45,12 +46,33 @@ function getDataFromDocs(docs) {
 
 //Create
 
-function addDocument() {
-  const data = {
-    name: 'Đỗ Hoàng Anh',
-    age: 18
-  }
-  db.collection('users').add(data)
+// function addDocument() {
+//     const data = {
+//     name: 'Đỗ Hoàng Anh',
+//     age: 18
+//     }
+//     db.collection('users').add(data)
+// }
+
+// addDocument();
+
+
+//Update
+function updateDocument() {
+    const idUpdate = 'MA9LsCXb5zNdK7S3mhCh';
+    const dataToUpdate = {
+        phones: firebase.firestore.FieldValue.arrayUnion('1123')
+    }
+    db.collection('users').doc(idUpdate).update(dataToUpdate);
 }
 
-addDocument();
+updateDocument();
+
+
+//Delete
+function deleteDocument() {
+    const idUpdate = 'MA9LsCXb5zNdK7S3mhCh';
+    db.collection('users').doc(idUpdate).delete();
+}
+
+deleteDocument();
